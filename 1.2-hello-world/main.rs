c79554d00ad9f1257f879d4ef13f642cd9da9399
matthras <matthew.y.p.mack@gmail.com>
2020-12-02 23:40:38 +1100

    added comment indicating ! macro

--- 1.2-hello-world/main.rs
+++ 1.2-hello-world/main.rs
@@ -1,7 +1,7 @@
 // main() function is similar in concept to the main() function in C++, Python, etc. - it's meant to be the very first function ran in the program
 
 fn main() {
-  println!("Hello World!");
+    println!("Hello World!"); // The ! indicates it's a Rust macro, instead of a function
 }
 
 // Compile by writing `rustc <name of rust file>`, then `./main' on WSL
